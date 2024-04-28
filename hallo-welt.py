88c54de4e57b72deec56216dd2190d52df8c4ae9
Patrick Scribus <patrick.scribus@ist-einmalig.de>
2024-04-28 10:15:21 +0200

    hinzufügen eines nonkonformen Hallo Welt

---
+++ hallo-welt.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+Autorin = 'Nicole'
+print('\n\nHallo Fremder.')
+Nutzerin = input('Wie lautet Dein Vorname, bitte? ')
+print(f'Danke {Nutzerin}! Die Autorin dieses kleinen Programmes heißt'
+      f' {Autorin} und sie wünscht Dir eine gute Zeit.\n\n')
